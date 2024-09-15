28964a12c17ed9e1eeaa8666dd70bd7b9253dca0
Abdurrahman <Abdrhmn.gh@gmail.com>
2024-09-15 14:16:30 +0700

    belajar membuat context, cancel, timeout dan deadline

--- go_belajar_context_test.go
+++ go_belajar_context_test.go
@@ -3,7 +3,9 @@ package belajargolanjutan
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestContext(t *testing.T) {
@@ -14,4 +16,104 @@ func TestContext(t *testing.T) {
 
 	todo := context.TODO()
 	fmt.Println(todo)
+}
+
+func TestCreateContext(t *testing.T) {
+	contextParent := context.Background()
+
+	// value context itu bersifat `Pair-value`
+	childA := context.WithValue(contextParent, "key1", "abdu")
+	childB := context.WithValue(contextParent, "key2", "eunha")
+
+	parentChildA := context.WithValue(childA, "childA1", "yerin")
+
+	fmt.Println(childB)
+	fmt.Println(parentChildA)
+	fmt.Println(parentChildA.Value("childA1")) // yerin
+	fmt.Println(parentChildA.Value("key2"))    // nil
+}
+
+// context with sinyal cancel
+func CreateCounter(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+		counter := 1
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Check total goroutine: ", runtime.NumGoroutine())
+
+	parentCtx := context.Background()
+	ctx, cancel := context.WithCancel(parentCtx)
+
+	destination := CreateCounter(ctx)
+
+	fmt.Println("Check total goroutine: ", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter: ", n)
+		if n == 10 {
+			break
+		}
+	}
+
+	cancel() // mengirim sinyal cancel ke context
+	time.Sleep(2 * time.Second)
+	fmt.Println("Check total goroutine: ", runtime.NumGoroutine())
+}
+
+// cancel context with timeout
+func TestContextWithCancelTimeout(t *testing.T) {
+	fmt.Println("Check total goroutine: ", runtime.NumGoroutine())
+
+	parentCtx := context.Background()
+	ctx, cancel := context.WithTimeout(parentCtx, 5*time.Second)
+
+	destination := CreateCounter(ctx)
+
+	defer cancel()
+
+	fmt.Println("Check total goroutine: ", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter: ", n)
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Println("Check total goroutine: ", runtime.NumGoroutine())
+}
+
+// context with deadline time
+func TestContextWithCancelDeadline(t *testing.T) {
+	fmt.Println("Check total goroutine: ", runtime.NumGoroutine())
+
+	parentCtx := context.Background()
+	ctx, cancel := context.WithDeadline(parentCtx, time.Now().Add(10*time.Minute)) // cancel dlm waktu 10 mnt
+
+	destination := CreateCounter(ctx)
+
+	defer cancel()
+
+	fmt.Println("Check total goroutine: ", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter: ", n)
+		time.Sleep(1 * time.Second)
+	}
+
+	fmt.Println("Check total goroutine: ", runtime.NumGoroutine())
 }
