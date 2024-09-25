35ee4e2ef936bcf9a5c6f0b9b97bdcee10626f63
Abdurrahman <Abdrhmn.gh@gmail.com>
2024-09-25 14:46:57 +0700

    belajar multiple embed, path matcher dan compilasi file embed

--- go_embed_test.go
+++ go_embed_test.go
@@ -1,6 +1,7 @@
 package belajargolanjutan
 
 import (
+	"embed"
 	_ "embed"
 	"fmt"
 	"io/fs"
@@ -24,4 +25,33 @@ func TestEmbedGambar(t *testing.T) {
 		panic(err)
 	}
 	fmt.Println("Berhasil embed gambar!")
+}
+
+// embed multiple files
+
+//go:embed files/a.txt
+//go:embed files/b.txt
+var files embed.FS
+
+func TestMultipleEmbed(t *testing.T) {
+	a, _ := files.ReadFile("files/a.txt")
+	fmt.Println(string(a))
+	b, _ := files.ReadFile("files/b.txt")
+	fmt.Println(string(b))
+}
+
+// path matcher
+
+//go:embed files/*.txt
+var path embed.FS
+
+func TestPathMatcher(t *testing.T) {
+	dirEntries, _ := path.ReadDir("files")
+	for _, entry := range dirEntries {
+		if !entry.IsDir() {
+			fmt.Println(entry.Name())
+			content, _ := path.ReadFile("files/" + entry.Name())
+			fmt.Println(string(content))
+		}
+	}
 }
